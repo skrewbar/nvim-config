af463a5a6c5a89469198cf0efcdd827ba6adc7c0
skrewbar <skurua1244@gmail.com>
2026-06-15 14:30:00 +0900

    🐛 Fix LuaSnip to load snippets when directory changed

--- lua/plugins/luasnip.lua
+++ lua/plugins/luasnip.lua
@@ -1,8 +1,26 @@
 return {
   "L3MON4D3/LuaSnip",
   opts = function(_, opts)
-    require("luasnip.loaders.from_vscode").lazy_load({
-      paths = { vim.fn.getcwd() .. "/.vscode" },
+    local vscode_loader = require("luasnip.loaders.from_vscode")
+
+    local function load_workspace_snippets(cwd)
+      local path = vim.fs.joinpath(cwd or vim.fn.getcwd(), ".vscode")
+      local stat = vim.uv.fs_stat(path)
+
+      if stat and stat.type == "directory" then
+        vscode_loader.lazy_load({
+          paths = { path },
+        })
+      end
+    end
+
+    load_workspace_snippets()
+
+    vim.api.nvim_create_autocmd("DirChanged", {
+      group = vim.api.nvim_create_augroup("workspace_luasnip", { clear = true }),
+      callback = function(event)
+        load_workspace_snippets(event.file)
+      end,
     })
 
     return opts
